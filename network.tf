ed68293659fa24fa9b0f65d2d625065a8ba8af4b
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-09-11 01:19:20 +0900

    修正版

--- network.tf
+++ network.tf
@@ -1,35 +1,37 @@
+locals {
+  region = "ap-northeast-1"
+  app    = "recruit-service-board"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "aws-ecs-terraform"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}c", "${local.region}d"]
+  azs             = ["${local.region}a", "${local.region}c"]
   public_subnets  = ["10.0.11.0/24", "10.0.12.0/24"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 
   public_subnet_names  = ["Public Subnet 1a", "Public Subnet 1c"]
-  private_subnet_names = ["Private Subnet 1a", "Private Subnet 1c", "${local.app} RDS Private Subnet 1d"]
+  private_subnet_names = ["Private Subnet 1a", "Private Subnet 1c"]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   enable_nat_gateway = true
   single_nat_gateway = true
+}
+
+resource "aws_subnet" "rds_subnet" {
+  vpc_id                  = module.vpc.vpc_id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "${local.region}d"
+  map_public_ip_on_launch = false
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-
-  public_subnet_tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-
-  private_subnet_tags = {
-    "subnet-1a" = { MapPublicIpOnLaunch = "true", type = "private" }
-    "subnet-1c" = { MapPublicIpOnLaunch = "true", type = "private" }
-    "subnet-1d" = { MapPublicIpOnLaunch = "false", type = "rds" }
+    Name                  = "${local.app} RDS Private Subnet 1d"
+    "MapPublicIpOnLaunch" = "false"
+    "Type"                = "rds"
   }
 }
