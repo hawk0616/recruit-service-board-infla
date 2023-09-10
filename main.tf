eb4ca977b3bb93eccff03af39662f88546dc2a8a
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-09-11 00:07:53 +0900

    修正

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 terraform {
-  # Assumes s3 bucket and dynamo DB table already set up
   backend "s3" {
     bucket         = "recruit-info-service-tfstate"
     key            = "terraform.tfstate"
@@ -7,17 +6,17 @@ terraform {
     dynamodb_table = "recruit-info-service-tfstate-locking"
     encrypt        = true
   }
-  
-   required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.0"
-   }
- }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
- provider "aws" {
-   region = local.region
- }
+provider "aws" {
+  region = local.region
+}
 
 
