89a1a4e5ffea8dbb383599a89ceff5346be55fa7
遠藤大志 <89571473+hawk0616@users.noreply.github.com>
2023-09-24 23:46:52 +0900

    Revert "ec2インスタンス"

--- security_group.tf
+++ security_group.tf
@@ -72,28 +72,3 @@ resource "aws_security_group" "rds" {
     Name = "${local.app}-rds"
   }
 }
-
-# For Bastion
-resource "aws_security_group" "bastion_sg" {
-  name        = "${local.app}-bastion"
-  description = "Allow SSH traffic"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${local.app}-bastion"
-  }
-}
