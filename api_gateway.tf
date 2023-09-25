a1726e5c3785c9c54ac037fd0353c95297ec16e9
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-09-26 02:19:24 +0900

    エンドポイントを叩かせlambdaを起動、rdsにアクセス

---
+++ api_gateway.tf
@@ -0,0 +1,39 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "${local.app}-api"
+  description = "API for triggering lambda"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "migrate"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.migrate_function.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.migrate_function.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
--- iam.tf
+++ iam.tf
@@ -50,3 +50,30 @@ resource "aws_iam_role_policy_attachment" "ecs_task_ssm_get_parameter" {
   policy_arn = aws_iam_policy.ssm_get_parameter.arn
   role       = aws_iam_role.ecs_task.name
 }
+
+# ForLambda
+# iam.tf
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "${local.app}-lambda-execution"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
