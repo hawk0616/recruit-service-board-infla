8d245e94e96a19813bf7f4592883ffd842b50451
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-10-26 01:15:45 +0900

    再構築

--- lambda.tf
+++ lambda.tf
@@ -1,22 +1,33 @@
-# resource "aws_lambda_function" "migrate_function" {
-#   function_name = "${local.app}-migrate"
-#   handler       = "index.handler"
-#   runtime       = "go1.x"
-#   role          = aws_iam_role.lambda_execution_role.arn
-#   filename      = "lambda_binary.zip"
+resource "aws_lambda_function" "migrate_function" {
+  function_name = "${local.app}-migrate"
+  handler       = "index.handler"
+  runtime       = "go1.x"
+  role          = aws_iam_role.lambda_execution_role.arn
+  filename      = "lambda_binary.zip"
 
-#   vpc_config {
-#     subnet_ids         = [aws_subnet.rds_subnet_a.id, aws_subnet.rds_subnet_d.id]
-#     security_group_ids = [aws_security_group.lambda_sg.id]
-#   }
-# }
+  vpc_config {
+    subnet_ids         = [aws_subnet.rds_subnet_a.id, aws_subnet.rds_subnet_cc.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+}
 
-# resource "aws_security_group_rule" "lambda_access_rds" {
-#   security_group_id = aws_security_group.lambda_sg.id
+resource "aws_security_group_rule" "lambda_access_rds" {
+  security_group_id = aws_security_group.lambda_sg.id
 
-#   type        = "ingress"
-#   from_port   = 3306
-#   to_port     = 3306
-#   protocol    = "tcp"
-#   cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
-# }
+  type        = "ingress"
+  from_port   = 3306
+  to_port     = 3306
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+resource "aws_security_group_rule" "lambda_from_apigw" {
+  security_group_id = aws_security_group.lambda_sg.id
+
+  type      = "ingress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  # 一旦全てのIPからのアクセスを許可
+  cidr_blocks = ["0.0.0.0/0"]
+}
--- network.tf
+++ network.tf
@@ -18,38 +18,39 @@
 #   single_nat_gateway = false
 # }
 
-# resource "aws_subnet" "rds_subnet_a" {
-#   vpc_id                  = module.vpc.vpc_id
-#   cidr_block              = "10.0.5.0/24"
-#   availability_zone       = "${local.region}a"
-#   map_public_ip_on_launch = false
+# RDSをaのAZに配置
+resource "aws_subnet" "rds_subnet_a" {
+  vpc_id                  = module.vpc.vpc_id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "${local.region}a"
+  map_public_ip_on_launch = false
 
-#   tags = {
-#     Name                  = "${local.app} RDS Private Subnet 1a"
-#     "MapPublicIpOnLaunch" = "false"
-#     "Type"                = "rds"
-#   }
-# }
+  tags = {
+    Name                  = "${local.app} RDS Private Subnet 1a"
+    "MapPublicIpOnLaunch" = "false"
+    "Type"                = "rds"
+  }
+}
 
-# resource "aws_subnet" "rds_subnet_d" {
-#   vpc_id                  = module.vpc.vpc_id
-#   cidr_block              = "10.0.6.0/24"
-#   availability_zone       = "${local.region}d"
-#   map_public_ip_on_launch = false
+# RDSをcのAZに配置
+resource "aws_subnet" "rds_subnet_c" {
+  vpc_id                  = module.vpc.vpc_id
+  cidr_block              = "10.0.6.0/24"
+  availability_zone       = "${local.region}c"
+  map_public_ip_on_launch = false
 
-#   tags = {
-#     Name                  = "${local.app} RDS Private Subnet 1d"
-#     "MapPublicIpOnLaunch" = "false"
-#     "Type"                = "rds"
-#   }
-# }
+  tags = {
+    Name                  = "${local.app} RDS Private Subnet 1c"
+    "MapPublicIpOnLaunch" = "false"
+    "Type"                = "rds"
+  }
+}
 
-# resource "aws_db_subnet_group" "my_db_subnet_group" {
-#   name       = "${local.app}-db-subnet-group"
-#   subnet_ids = [aws_subnet.rds_subnet_a.id, aws_subnet.rds_subnet_d.id]
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "${local.app}-db-subnet-group"
+  subnet_ids = [aws_subnet.rds_subnet_a.id, aws_subnet.rds_subnet_c.id]
 
-#   tags = {
-#     Name = "${local.app}-db-subnet-group"
-#   }
-# }
-
+  tags = {
+    Name = "${local.app}-db-subnet-group"
+  }
+}
